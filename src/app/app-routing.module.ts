6062046d0d8eb311c2b64783854379ac50ba2370
Lesiv Lyubomir <lyubas84@mail.ru>
2023-01-22 11:58:39 +0100

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingCardComponent } from './components/shopping-card/shopping-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'product/:id', component: ProductComponent},
+  {path: 'shopping-card', component: ShoppingCardComponent},
+  {path:"**", redirectTo: "", component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
